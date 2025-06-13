31976fcd7aa63c0a8a7bdfcabc2e6cd0d1ddae70
Wai Yan <waiyanoo.mgk@gmail.com>
2025-06-13 15:32:23 +0800

    add loading bar

--- functions/index.js
+++ functions/index.js
@@ -8,14 +8,18 @@
  */
 
 // import { onDocumentCreated, onDocumentUpdated } from "firebase-functions/v2/firestore";
-import { initializeApp } from 'firebase-admin/app';
+import { initializeApp } from "firebase-admin/app";
+import { getFirestore } from 'firebase-admin/firestore';
 // import  algoliasearch  from 'algoliasearch';
+import { onSchedule } from "firebase-functions/v2/scheduler";
+import { Timestamp } from "firebase-admin/firestore";
+import { doc, serverTimestamp, setDoc } from "firebase/firestore";
 
-initializeApp();
-
-const appId = "LUQUCJ1X7P";
-const adminKey = "26bec2fffc79ba517291e2d15fea6dc4";
-const indexName = 'Dragon';
+const adminApp = initializeApp();
+const db = getFirestore(adminApp);
+// const appId = "LUQUCJ1X7P";
+// const adminKey = "26bec2fffc79ba517291e2d15fea6dc4";
+// const indexName = "Dragon";
 
 // const client = algoliasearch(appId, adminKey);
 // const index = client.initIndex(indexName);
@@ -37,3 +41,59 @@ const indexName = 'Dragon';
 //   await index.saveObject(data);
 //   console.log('Document added to Algolia:', event.params.docId);
 // });
+
+export const monthlyReport = onSchedule(
+  {
+    schedule: "0 5 1 * *", // Every 1st day of the month at midnight
+    timeZone: "Asia/Yangon", // Set your timezone
+  },
+  async (_) => {
+    const now = new Date();
+
+    // Compute the previous month
+    const year = now.getMonth() === 0 ? now.getFullYear() - 1 : now.getFullYear();
+    const month = now.getMonth() === 0 ? 12 : now.getMonth(); // 1-12
+
+    const start = new Date(year, month - 1, 1); // Start of last month
+    const end = new Date(year, month, 1); // Start of the current month
+
+    const startTimestamp = Timestamp.fromDate(start);
+    const endTimestamp = Timestamp.fromDate(end);
+
+    const snapshot = await db
+      .collection("orders")
+      .where("createdAt", ">=", startTimestamp)
+      .where("createdAt", "<", endTimestamp)
+      .get();
+
+    const reportData = {};
+    snapshot.forEach((doc) => {
+      const { brand, amount = 0, createdBy } = doc.data();
+      if (!brand) return;
+
+      // Initialize a brand group
+      if (!reportData[brand]) {
+        reportData[brand] = {
+          totalAmount: 0,
+          orderCount: 0,
+          createdByCount: {}, // createdBy: count
+        };
+      }
+
+      // Update totals
+      reportData[brand].totalAmount += +amount;
+      reportData[brand].orderCount += 1;
+      reportData.createAt = serverTimestamp();
+
+      // Count per createdBy
+      if (createdBy) {
+        if (!reportData[brand].createdByCount[createdBy]) {
+          reportData[brand].createdByCount[createdBy] = 0;
+        }
+        reportData[brand].createdByCount[createdBy] += 1;
+      }
+    });
+    const reportId = `${year}_${month.toString().padStart(2, "0")}`;
+    await setDoc(doc(db, "monthlyReports", reportId), reportData);
+  }
+);
